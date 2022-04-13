06449e613a153bc92f09a878a81c53c44559b76f
Renaud Jenny <renaud.jenny@gmail.com>
2022-04-13 22:11:34 +0100

    feat(Points): add visible points on the Drawing panel
    * Also experiment the code update

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
             HStack {
                 ZStack {
                     ZStack {
-                        DrawingPanel()
+                        DrawingPanel(code: $code)
                         image?
                             .resizable()
                             .aspectRatio(contentMode: .fit)
@@ -43,8 +43,69 @@ struct ContentView: View {
 }
 
 struct DrawingPanel: View {
+    @State private var tapGesturePoint: CGPoint = .zero
+    @State private var points: [CGPoint] = [] {
+        didSet { updateCode() }
+    }
+    @Binding var code: String
+
     var body: some View {
-        Color.white
+        ZStack {
+            Color.white
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.interactiveSpring()) { tapGesturePoint = value.location }
+                        }
+                        .onEnded { _ in
+                            points.append(tapGestureStandardizedPosition)
+                        }
+                )
+
+            if tapGesturePoint != .zero {
+                Circle()
+                    .frame(width: 10, height: 10)
+                    .position(tapGestureStandardizedPosition)
+            }
+
+            ForEach(Array(points.enumerated()), id: \.offset) { offset, point in
+                Circle()
+                    .frame(width: 5, height: 5)
+                    .position(point)
+            }
+        }
+    }
+
+    private var tapGestureStandardizedPosition: CGPoint {
+        var standardizedPoint = tapGesturePoint
+        if standardizedPoint.x < 0 {
+            standardizedPoint.x = 0
+        }
+        if standardizedPoint.y < 0 {
+            standardizedPoint.y = 0
+        }
+        if standardizedPoint.x > 800 {
+            standardizedPoint.x = 800
+        }
+        if standardizedPoint.y > 800 {
+            standardizedPoint.y = 800
+        }
+        return standardizedPoint
+    }
+
+    private func updateCode() {
+        code = points.map { point in
+            let x = abs(point.x - 400) * 10/8
+            let y = abs(point.y - 400) * 10/8
+            let xSign = point.x > 400 ? "+" : "-"
+            let ySign = point.y > 400 ? "-" : "+"
+            return """
+            path.addLine(to: CGPoint(
+                x: rect.midX \(xSign) width * \(Int(x.rounded()))/1000,
+                y: rect.midY \(ySign) width * \(Int(y.rounded()))/1000
+            )
+            """
+        }.joined(separator: "\n")
     }
 }
 
