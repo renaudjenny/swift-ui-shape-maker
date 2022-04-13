672f94fab054e13826c61260e0a889dfece450df
Renaud Jenny <renaud.jenny@gmail.com>
2022-04-13 18:25:31 +0100

    Initial Commit

---
+++ Shared/SwiftUI_Shape_MakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Shape_MakerApp.swift
+//  Shared
+//
+//  Created by Renaud JENNY on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Shape_MakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
