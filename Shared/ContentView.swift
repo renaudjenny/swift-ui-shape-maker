738f8282a8ff3ce7edef63fa9568417b739390ad
Renaud Jenny <renaud.jenny@gmail.com>
2022-04-18 08:27:16 +0100

    refactor(Code View): put the Code View in its own file

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -56,65 +56,11 @@ struct ContentView: View {
                     }
                 }
 
-                ZStack {
-                    if isCodeInEditionMode {
-                        TextEditor(text: code)
-                            .font(.body.monospaced())
-                            .multilineTextAlignment(.leading)
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                    } else {
-                        ScrollView {
-                            HStack {
-                                VStack(alignment: .leading, spacing: 0) {
-                                    Text(codeHeader.codeFormatted)
-                                    ForEach(Array(pathElements.enumerated()), id: \.offset) { offset, element in
-                                        HStack {
-                                            Text(element.code.codeFormatted(extraIndentation: 2))
-                                            if hoveredOffsets.contains(offset) {
-                                                Button("Remove", role: .destructive) {
-                                                    pathElements.remove(at: offset)
-                                                }
-                                                .padding(.horizontal)
-                                            }
-                                        }
-                                        .background {
-                                            if hoveredOffsets.contains(offset) {
-                                                RoundedRectangle(cornerRadius: 8)
-                                                    .fill(Color.white)
-                                                    .shadow(color: .black, radius: 4)
-                                            } else {
-                                                RoundedRectangle(cornerRadius: 8)
-                                                    .fill(Color.white)
-                                                    .padding()
-                                            }
-                                        }
-                                        .contentShape(Rectangle())
-                                        .onHover { isHovered in
-                                            withAnimation(.easeInOut) {
-                                                if isHovered {
-                                                    hoveredOffsets.insert(offset)
-                                                } else {
-                                                    hoveredOffsets.remove(offset)
-                                                }
-                                            }
-                                        }
-                                    }
-                                    Text(codeFooter)
-                                }
-                                .padding(.horizontal, 5)
-                                Spacer()
-                            }
-                        }
-                        .multilineTextAlignment(.leading)
-                        .frame(maxWidth: .infinity)
-                        .font(.body.monospaced())
-                        .background(Color.white)
-                        .padding()
-                    }
-                }.onTapGesture {
-                    isCodeInEditionMode = true
-                }
+                CodeView(
+                    pathElements: $pathElements,
+                    hoveredOffsets: $hoveredOffsets,
+                    isInEditionMode: $isCodeInEditionMode
+                )
             }
         }
     }
@@ -126,34 +72,6 @@ struct ContentView: View {
         pictureTaker?.outputImage().map { image = Image(nsImage: $0) }
         #endif
     }
-
-    private var code: Binding<String> {
-        Binding<String>(
-            get: { [
-                codeHeader,
-                pathElements.map(\.code).joined(separator: "\n"),
-                codeFooter,
-            ].joined(separator: "\n").codeFormatted },
-            set: { _, _ in }
-        )
-    }
-
-    private var codeHeader: String { """
-    import SwiftUI
-
-    struct MyShape: Shape {
-        func path(in rect: CGRect) -> Path {
-            var path = Path()
-            let width = min(rect.width, rect.height)
-    """}
-
-    // swiftlint: disable indentation_width
-    private var codeFooter: String { """
-            return path
-        }
-    }
-    """ }
-    // swiftlint: enable indentation_width
 }
 
 struct ContentView_Previews: PreviewProvider {
