954519f5bff05f331446310a7113284682ba5b2e
Renaud Jenny <renaud.jenny@gmail.com>
2022-05-23 21:30:47 +0100

    fix(Extensions): update `Collection+Safe` extension to use correct index

--- Shared/Extensions/Collection+Safe.swift
+++ Shared/Extensions/Collection+Safe.swift
@@ -1,6 +1,6 @@
 extension Collection where Index == Int {
     subscript(safe index: Index) -> Element? {
-        guard index > 0 && index < count else { return nil }
+        guard index >= 0 && index < count else { return nil }
         return self[index]
     }
 }
